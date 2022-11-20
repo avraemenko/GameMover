110e7091a5cd6a33b24247b600c5843c49778e98
Kateryna Avramenko <kavramenko2000@gmail.com>
2022-11-20 17:06:35 +0100

    Initial Commit

---
+++ XOMover/XOMover.h
@@ -0,0 +1,18 @@
+//
+//  XOMover.h
+//  XOMover
+//
+//  Created by Kateryna Avramenko on 20.11.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XOMover.
+FOUNDATION_EXPORT double XOMoverVersionNumber;
+
+//! Project version string for XOMover.
+FOUNDATION_EXPORT const unsigned char XOMoverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XOMover/PublicHeader.h>
+
+
